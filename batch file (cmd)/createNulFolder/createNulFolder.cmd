ee118e7a73c6efe430c172a08657f98631f92b38
r8btx <60275558+r8btx@users.noreply.github.com>
2021-06-17 07:45:40 +0000

    Minor improvements

--- batch file (cmd)/createNulFolder/createNulFolder.cmd
+++ batch file (cmd)/createNulFolder/createNulFolder.cmd
@@ -26,7 +26,7 @@ attrib +h +s "\\.\%~dp0nul"
 
 
 
-echo.End of File.
+echo/End of File.
 pause
 exit /b
 
@@ -34,7 +34,7 @@ exit /b
 
 :deleteNul
 rd Link2nul >nul 2>&1
-rd /s /q "\\.\%~dp0nul" && echo.Removal complete. && pause && exit /b 0
+rd /s /q "\\.\%~dp0nul" && echo/Removal complete. && pause && exit /b 0
 exit /b 1
 
 :createSymLink
@@ -44,13 +44,13 @@ mklink /d "Link2nul" "\\.\%~dp0nul" && pause && exit /b 0
 exit /b 1
 
 :writeIni
-echo.%~1>>"\\.\%~dp0nul\desktop.ini"
+echo/%~1>>"\\.\%~dp0nul\desktop.ini"
 exit /b
 
 :elevate
 whoami /groups |find " S-1-16-12288 " >nul 2>&1 && exit /b 0
-echo.To create a symbolic link, admin privilege is required.
-echo.Requesting administrative privilege...
-powershell Start-Process "%~s0" -Verb runAs
-if %ErrorLevel% neq 0 echo.User denied the request. && pause && exit /b 1
+echo/To create a symbolic link, admin privilege is required.
+echo/Requesting administrative privilege...
+powershell Start-Process '%~s0' -Verb runAs
+if %ErrorLevel% neq 0 echo/User denied the request. && pause && exit /b 1
 exit /b 2
