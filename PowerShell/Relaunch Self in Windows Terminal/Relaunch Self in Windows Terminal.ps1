bf1b7c4a347439613ba034b26525ccde35279789
r8btx <60275558+r8btx@users.noreply.github.com>
2023-06-11 15:17:07 +0400

    Relaunch Self in Windows Terminal

---
+++ PowerShell/Relaunch Self in Windows Terminal/Relaunch Self in Windows Terminal.ps1
@@ -0,0 +1,14 @@
+﻿# Launch Self in Windows Terminal
+
+$CodeString = ""
+$CodeString += "Set-ExecutionPolicy Bypass -Scope Process;"
+$CodeString += "&'$PSCommandPath';"
+$Code = [scriptblock]::Create($CodeString)
+
+if ($env:WT_PROFILE_ID) {
+  Write-Host "Launched in Windows Terminal"
+  pause
+} else {
+  if (Get-Command wt.exe -ErrorAction SilentlyContinue) { wt.exe -p "Windows PowerShell" powershell.exe -command $Code }
+  if (Get-Command wdt.exe -ErrorAction SilentlyContinue) { wdt.exe -p "Windows PowerShell" powershell.exe -command $Code }
+}
